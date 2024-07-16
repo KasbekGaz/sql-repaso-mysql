ab6f82817d0366a2fdd05d8adb9d405224e512a2
KasbekGaz <oswa.laza.flores@hotmail.com>
2024-07-15 21:10:40 -0700

    Subiendo el resto

---
+++ ComandosqldeBDcicilsoft.sql
@@ -0,0 +1,11 @@
+/*INSERT into proveedor(id,nombre_comercial,razon_social,telefono,correo,RFC,descripcion)
+values(1,'La Cochinilla','Convencinal ferreteria','9612345678', 'chanchi@gmail.com','HGFTEUYD45678','Descripcion x');
+ (-- Aqui se agregio un proveedor --)
+*/
+/* insertamos un dato a banca del proveedor 1
+insert INTO banca(id,banco,cuenta_banco,clave_banco,proveedor_id)
+value (1,'santander','123HYASDHU','12345UYHGTFS',1);
+*/
+/*
+insert INTO banca(id,banco,cuenta_banco,clave_banco,proveedor_id)
+value (2,'BBVA','12345ERTY','JKLHMASL0987',1); */
